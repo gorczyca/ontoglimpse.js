edbd78cc6e9bed8ce92883e691ea284d9d19fe82
Piotr Gorczyca <piotr.gorczyca@tu-dresden.de>
2024-08-28 16:50:27 +0200

    Revert changes back

--- src/ontoGlimpse.js
+++ src/ontoGlimpse.js
@@ -64,9 +64,9 @@ class Entity {
         return this.__dataProperty(this.node, property, object)
     }
 
-    booleanProperty(property, object) {
-        return dataProperty(property, object) === '1' ? true : false
-    }
+    // booleanProperty(property, object) {
+    //     return dataProperty(property, object) === '1' ? true : false
+    // }
 
 
 
@@ -312,7 +312,7 @@ class OntoGlimpse {
             title: oe.dataProperty(DCT('title'), undefined),
             creators: this.personsInfo(on, DCT('creator')),
             contributors: this.personsInfo(on, DCT('contributor')),
-            customSwitch1: oe.booleanProperty(ONTOGS('customSwitch1'), undefined)
+            customSwitch1: oe.dataProperty(ONTOGS('customSwitch1'), undefined)
             // title: oe.dataProperty(DCT('title'), undefined)
         })
     }
